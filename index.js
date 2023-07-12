4e8db7117a4215ba34fd907850ca580dbac5f955
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-07-12 11:21:10 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+  }
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+  }
+
+  function appendCat(name) {
+    const newArray = cats.slice(); // Create a new array by copying the cats array
+    newArray.push("Broom"); // Append the new cat to the new array
+    return newArray; // Return the new array
+  }
+
+  function prependCat(name) {
+    const newArray = cats.slice();
+    newArray.unshift("Arnold");
+    return newArray; 
+  }
+
+  function removeLastCat(name) {
+    const newArray = cats.slice(0, -1);
+    return newArray; 
+  }
+
+  function removeFirstCat(name) {
+    const newArray = cats.slice(1);
+    return newArray; 
+  }
